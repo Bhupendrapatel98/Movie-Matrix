f4328e0e00c909d20af22fbf3e6438c1a4056030
bhupendrasmt <bhupendrasmt@gmail.com>
2024-05-30 11:52:47 +0530

    image banner slider ui done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.accompanist.pager)
+    debugImplementation(libs.accompanist.pager.indicators)
 }
