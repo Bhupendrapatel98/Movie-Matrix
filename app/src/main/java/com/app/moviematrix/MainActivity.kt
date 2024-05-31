00cddd748809da2c954c2d4bfe384a95fb64da06
bhupendrasmt <bhupendrasmt@gmail.com>
2024-05-31 16:58:20 +0530

    code refector

--- app/src/main/java/com/app/moviematrix/MainActivity.kt
+++ app/src/main/java/com/app/moviematrix/MainActivity.kt
@@ -5,29 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
@@ -56,43 +44,8 @@ class MainActivity : ComponentActivity() {
                         .padding(horizontal = 15.dp)
                         .verticalScroll(rememberScrollState())
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 15.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = "Movie Stack",
-                            color = Color.White,
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        Image(
-                            Icons.Rounded.Search,
-                            contentDescription = "Search",
-                            colorFilter = ColorFilter.tint(Color.White),
-                            modifier = Modifier.size(32.dp),
-                        )
-                    }
-
+                    Header()
                     SliderBanner()
-                    Row(
-                        modifier = Modifier.padding(bottom = 10.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = "Trending",
-                            color = Color.White,
-                            fontSize = 21.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Spacer(modifier = Modifier.width(5.dp))
-                        Text(text = "Person", color = Color.Gray, fontSize = 16.sp)
-                        Spacer(modifier = Modifier.weight(1f))
-                        Text(text = "More", color = Color.Red, fontSize = 16.sp)
-                    }
                     TrendingPersonList()
                     TrendingMovies()
                     PopularMovies()
