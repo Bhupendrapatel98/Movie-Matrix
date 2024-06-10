0f4dafda2c66bbad3d88f5fb582aeb2488f1d310
bhupendrasmt <bhupendrasmt@gmail.com>
2024-06-10 16:01:00 +0530

    write unit test for trendingpersonViewModel
    - write test case for sucess
    
    - write test case for failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //unit testing
     testImplementation(libs.junit)
+    testImplementation(libs.mockito)
+    testImplementation(libs.kotlinx.coroutines.test)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
