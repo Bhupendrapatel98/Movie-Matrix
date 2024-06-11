8ff4a682a48d1115e7d1077ebbaf9e5ba8a41f96
bhupendrasmt <bhupendrasmt@gmail.com>
2024-06-11 11:18:28 +0530

    implemented firebase
    - implemented firebase analytics
    
    added retry funtion in flow when we get IOExceptions

--- app/src/main/java/com/app/moviematrix/domain/use_case/TrendingPersonUseCase.kt
+++ app/src/main/java/com/app/moviematrix/domain/use_case/TrendingPersonUseCase.kt
@@ -6,12 +6,16 @@ import com.app.moviematrix.utills.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.retry
+import java.io.IOException
 import javax.inject.Inject
 
 class TrendingPersonUseCase @Inject constructor(private val repository: TrendingPersonRepository) {
     operator fun invoke(apiKey: String): Flow<Resource<TrendingPerson>> = flow {
         emit(Resource.loading())
         emit(Resource.success(repository.getTrendingPerson(apiKey)))
+    }.retry(1) { e ->
+        e is IOException
     }.catch {
         emit(Resource.failed(it.message.toString()))
     }
