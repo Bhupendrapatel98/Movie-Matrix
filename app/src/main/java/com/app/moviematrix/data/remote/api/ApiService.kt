694074cf4b8fdac2084d88273184b2b19cd8bb14
bhupendrasmt <bhupendrasmt@gmail.com>
2024-06-11 13:15:22 +0530

    Implementd linting
    implemented linting for code formating,structured and coding style

--- app/src/main/java/com/app/moviematrix/data/remote/api/ApiService.kt
+++ app/src/main/java/com/app/moviematrix/data/remote/api/ApiService.kt
@@ -5,7 +5,8 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-
     @GET("trending/person/day")
-    suspend fun getTrendingPerson(@Query("api_key") key: String) : TrendingPerson
+    suspend fun getTrendingPerson(
+        @Query("api_key") key: String,
+    ): TrendingPerson
 }
--- app/src/main/java/com/app/moviematrix/utills/Resource.kt
+++ app/src/main/java/com/app/moviematrix/utills/Resource.kt
@@ -2,12 +2,16 @@ package com.app.moviematrix.utills
 
 sealed class Resource<T> {
     class Loading<T> : Resource<T>()
+
     class Success<T>(val data: T) : Resource<T>()
-    class Failed<T>(val message:String)  : Resource<T>()
+
+    class Failed<T>(val message: String) : Resource<T>()
 
     companion object {
         fun <T> loading() = Loading<T>()
+
         fun <T> success(data: T) = Success(data)
+
         fun <T> failed(message: String) = Failed<T>(message)
     }
 }
