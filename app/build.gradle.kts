3f49b2f527b6aedb1571357dadfe3274c09f5b48
bhupendrasmt <bhupendrasmt@gmail.com>
2024-06-15 13:30:37 +0530

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.app.moviematrix"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
