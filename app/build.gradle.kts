bc3ee4d4fe9cd2d3f6b84950e01a01c5409bd63f
bhupendrasmt <bhupendrasmt@gmail.com>
2024-06-15 16:00:58 +0530

    cicd automation done completely

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.app.moviematrix"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
